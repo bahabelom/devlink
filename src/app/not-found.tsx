3b3de98ea99759702fdeeba6278b723d6a38c324
bahabelom <bahabelomgebremedhn2@gmail.com>
2025-04-05 13:21:38 +0300

    feat: build error fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 export default function NotFound() {
     return (
       <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-center">
         <h1 className="text-5xl font-bold text-red-500">404</h1>
         <p className="text-xl mt-4">Page Not Found 😢</p>
-        <a href="/" className="mt-6 text-blue-600 underline">
+        <Link href="/" className="mt-6 text-blue-600 underline">
           Go Back Home
-        </a>
+        </Link>
       </div>
     );
   }
