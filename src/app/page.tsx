91a64d48738b0a7afdee5ec5f79dcbe010e05d76
bahabelom <bahabelomgebremedhn2@gmail.com>
2025-04-05 20:44:35 +0300

    modified layout to use inter  font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
+      <h1 className= "text-4xl font-inter font-bold text-blue-600">
         Welcome to DevLink 🌐
       </h1>
     </div>
